c5b49afc603e765c7fd0469bd4d05cc0e5aca919
Odhran7 <russell.odhran@gmail.com>
2023-01-08 18:33:57 +0000

    Initial Commit - Basic Set up

---
+++ main.js
@@ -0,0 +1,4 @@
+import { CeramicClient } from '@ceramicnetwork/http-client';
+import { EthereumAuthProvider } from '@ceramicnetwork/blockchain-utils-linking';
+import { DIDDataStore } from '@glazed/did-datastore';
+import { DIDSession } from '@glazed/did-session';
