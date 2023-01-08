2664261579ce35aa525a1d4b12f05830faf7bc49
Odhran7 <russell.odhran@gmail.com>
2023-01-08 19:42:55 +0000

    Styling

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ import { CeramicClient } from '@ceramicnetwork/http-client';
 import { EthereumAuthProvider } from '@ceramicnetwork/blockchain-utils-linking';
 import { DIDDataStore } from '@glazed/did-datastore';
 import { DIDSession } from '@glazed/did-session';
+
+const profileForm = document.getElementById('profile-form');
+const walletBtn = document.getElementById('connect-wallet');
+const profileName = document.getElementById('name');
+const profileGender = document.getElementById('gender');
+const profileCountry = document.getElementById('country');
+const submitBtn = document.getElementById('submit-btn');
